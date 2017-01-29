052cba15a522a381a0a4189fa60c2bdb6289bf15
Sim Boon Long <simboonlong@gmail.com>
2017-01-29 21:28:30 +0800

    UPDATE
    - offcanvas version 2
    - remove tweenmax dependancy
    - remove bootstrap breakpoints dependancy
    - css-based animations
    - add overlapping menu option

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## offcanvas.js
 Yet another offcanvas solution...
 
-1. requires Jquery, TweenMax.
-2. manually tested to work on latest version of Chrome, Firefox, Safari and IE.
+1. Requires Jquery.
+2. Manually tested to work on latest version of Chrome, Firefox, Safari and IE.
 
 
 ## Demo
@@ -17,37 +17,38 @@ There are many offcanvas plugins out there, but I need a plugin that tackles mai
 
 1. Basic responsiveness + works in mobile.
 2. Position fixed elements within transformed offcanvas element.
-3. Preferably, both content and menu remains scrollable when in active state.
-4. Minimal code.
+3. Choice between underlaying menu and overlapping menu.
+4. Preferably, both content and menu remains scrollable when in active state.
+5. Separation of concerns (css, js).
 
 
 ## Here's how
 
-1. Breakpoints based on Bootstrap 3.3.* used.
-2. Can't defy this. Apply `.oc-slide` class to slide the fixed element as well.
-3. Uses native scrollbar. Nothing fancy. `-webkit-overflow-scrolling: touch` for menu in mobile.
-4. Tried to, but not without prerequisites: jQuery + TweenMax.
-
+1. Apply breakpoints implementation on `$oc-menu-width` as accordingly.
+2. Apply `.oc-slide` css class to slide element(s).
+3. Default is underlaying menu. For overlapping menu, apply `.oc-menu-overlap` css class to `.oc-menu` element(s).
+4. Uses native scrollbar. Nothing fancy. `-webkit-overflow-scrolling: touch` for menu in mobile.
+5. Data attributes used as triggers for JavaScript. Transitions/animations by toggling css classes.
 
 ## Usage
 
 Follow the structure in index.html. Import the css styles, otherwise scss version is available too. Notice that offcanvas.js is written loosely. Please feel free to modify to your needs.
 
-Basic options in offcanvas.js are:
+To initialize:
 
 ```
-var offcanvas = new OffCanvas({
-    throttleThreshold : 50, // control window resize throttle, updates menu trigger's position
-    slideDuration : 0.7, // sliding time (TweenMax)
-    slideEase : 'Expo.easeOut' // sliding ease type (TweenMax)
-});
+
+OffCanvas.init();
 
 ```
 
 To change left/right direction of the offcanvas slide:
 
-1. Apply `.oc-menu-trigger-left` or `.oc-menu-trigger-right` class to `#oc-menu-trigger` element in the html.
-2. Apply `.oc-menu-left` or `.oc-menu-right` class to `#oc-menu` element in the html.
+1. Apply either `data-oc-menu-trigger="left"` or `data-oc-menu-trigger="left"` data attribute to both `.oc-menu` and `.oc-menu-trigger` elements in the html.
+
+To use overlapping menu:
+
+1. Apply `.oc-menu-overlap` css class to `.oc-menu` element(s) in the html.
 
 
 ## License
--- scripts/offcanvas.js
+++ scripts/offcanvas.js
@@ -1,116 +1,76 @@
-(function( window, document, undefined ){
-    'use strict';
 
-    var HTML = $('html');
+/*!
+modal.js - http://simboonlong.com
+Licensed under the MIT license
+
+Copyright (c) 2017 Sim Boon Long
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+var OffCanvas = (function() {
+
+    // settings
     var ocAll = $('html, body, #oc-main, #oc-container');
     var ocMain = $('#oc-main');
+    var ocOverlay = $('.oc-overlay');
     var ocContainer = $('#oc-container');
     var ocContent = $('#oc-content');
-    var ocMenu = $('#oc-menu');
-    var isOcRight = ocMenu.hasClass('oc-menu-right');
-    var isOcLeft = ocMenu.hasClass('oc-menu-left');
+    var ocMenu = $('.oc-menu');
+    var ocMenuLeft = $('.oc-menu[data-oc-menu-trigger="left"]');
+    var ocMenuRight = $('.oc-menu[data-oc-menu-trigger="right"]');
     var ocSlide = $('.oc-slide');
-    var ocMenuTrigger = $('#oc-menu-trigger');
+    var ocMenuTrigger = $('.oc-menu-trigger');
 
-    function OffCanvas( options ){
-        this.throttleThreshold = options.throttleThreshold;
-        this.slideDuration = options.slideDuration;
-        this.slideEase = options.slideEase;
-        this.initialize();
-    }
-
-    OffCanvas.prototype.throttle = function(func, wait, options) {
-        var context, args, result;
-        var timeout = null;
-        var previous = 0;
-        if (!options) options = {};
-        var later = function() {
-            previous = options.leading === false ? 0 : Date.now();
-            timeout = null;
-            result = func.apply(context, args);
-            if (!timeout) context = args = null;
-        };
-        return function() {
-            var now = Date.now();
-            if (!previous && options.leading === false) previous = now;
-            var remaining = wait - (now - previous);
-            context = this;
-            args = arguments;
-            if (remaining <= 0 || remaining > wait) {
-                if (timeout) {
-                    clearTimeout(timeout);
-                    timeout = null;
-                }
-                previous = now;
-                result = func.apply(context, args);
-                if (!timeout) context = args = null;
-            } else if (!timeout && options.trailing !== false) {
-                timeout = setTimeout(later, remaining);
-            }
-            return result;
-        };
+    var deactivate = function() {
+        ocMain.removeClass('is-active is-opened-left is-opened-right');
+        ocMenu.removeClass('is-slide-left is-slide-right');
+        ocSlide.removeClass('is-slide-left is-slide-right');
     };
 
-    OffCanvas.prototype.updatePosition = function() {
-        if ( ocMain.hasClass('is-active') ) {
-            if (isOcRight) {
-                TweenMax.set( ocSlide, {x:-ocMenu.outerWidth()});
-            } else {
-                TweenMax.set( ocSlide, {x:ocMenu.outerWidth()});
-            }
-        }
-    };
+    var activate = function() {
 
-    OffCanvas.prototype.checkOcHeight = function() {
-        if ( ocContent.outerHeight() < $(window).innerHeight() ) {
-            TweenMax.set( ocAll, {height:'100%'}); // lengthen background height to hide insufficient content
-        } else {
-            TweenMax.set( ocAll, {height:'auto'});
-        }
-    };
+        var trigger = $(this).attr('data-oc-menu-trigger');
 
-    OffCanvas.prototype.slide = function() {
-        this.checkOcHeight();
         if (!ocMain.hasClass('is-active')) {
-
-            TweenMax.set( ocMenu, {display:'block'});
-            if (isOcRight) {
-                TweenMax.to( ocSlide, this.slideDuration, {x:-ocMenu.outerWidth(), ease:this.slideEase,
-                    onComplete: function() {
-                        TweenMax.set( ocMenu, {zIndex:1});
-                    }
-                });
-            } else {
-                TweenMax.to( ocSlide, this.slideDuration, {x:ocMenu.outerWidth(), ease:this.slideEase,
-                    onComplete: function() {
-                        TweenMax.set( ocMenu, {zIndex:1});
-                    }
-                });
-            }
-            ocMain.addClass('is-active');
-
-        } else {
-
-            TweenMax.set( ocMenu, {zIndex:0});
-            TweenMax.to( ocSlide, this.slideDuration, {x:0, ease:this.slideEase,
-                onComplete: function() {
-                    TweenMax.set( ocMenu, {display:'none'});
+            if ( trigger === 'left' ) {
+                if ( ocMenuLeft.hasClass('oc-menu-overlap') ) {
+                    ocSlide.not('#oc-container').not('.oc-overlay').not('.oc-menu-trigger').addClass('is-slide-left');
+                } else {
+                    ocSlide.addClass('is-slide-left');
                 }
-            });
-            ocMain.removeClass('is-active');
+                ocMenuLeft.addClass('is-slide-left');
+            } else {
+                if ( ocMenuRight.hasClass('oc-menu-overlap') ) {
+                    ocSlide.not('#oc-container').not('.oc-overlay').not('.oc-menu-trigger').addClass('is-slide-right');
+                } else {
+                    ocSlide.addClass('is-slide-right');
+                }
+                ocMenuRight.addClass('is-slide-right');
+            }
+            ocMain.addClass('is-active is-opened-'+trigger+'');
         }
+
     };
 
-    OffCanvas.prototype.initialize = function() {
-        var throttled = this.throttle( this.updatePosition, this.throttleThreshold );
-        $(window).on('resize', throttled );
-        ocMenuTrigger.on('click', this.slide.bind(this));
+
+    var init = function() {
+
+        ocMenuTrigger.on('click', activate );
+        ocOverlay.on('click', deactivate );
+
     };
 
-    var offcanvas = new OffCanvas({
-        throttleThreshold : 50, // control window resize throttle, updates menu trigger's position
-        slideDuration : 0.7, // sliding time (TweenMax)
-        slideEase : 'Expo.easeOut' // sliding ease type (TweenMax)
-    });
+    return {
+        init : init
+    };
 
-})( this, this.document );
+})();
+
+
+
+OffCanvas.init();
